3dafdab69f4fbce9b0fcda848e22cf89ceb3b366
izofiya <izofiya@mail.ru>
2020-04-12 00:52:09 +0300

    delete files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Radium, {StyleRoot} from "radium";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { id: "111", name: "Zoi", age: "33" },
+      { id: "112", name: "Alex", age: "38" },
+      { id: "122", name: "Dem", age: "48" },
+    ],
+    otherState: "some other value",
+    showPersons: false,
+  });
+  const [showPersonsState, setShowPersonsState] = useState({
+    showPersons: false,
+  });
+
+  const nameChangedHandler = (event, id) => {
+    const personIndex = personsState.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...personsState.persons[personIndex] };
+    person.name = event.target.value;
+    const persons = [...personsState.persons];
+    persons[personIndex] = person;
+
+    setPersonsState({
+      persons
+    });
+  };
+
+  const togglePersonsHandler = () => {
+    const doesShow = showPersonsState.showPersons;
+    setShowPersonsState({
+      showPersons: !doesShow,
+    });
+  };
+
+  const deletePersonHandler = (personIndex) => {
+    const persons = [...personsState.persons];
+    persons.splice(personIndex, 1); // starting at index position, delete 1 element and return the changed array
+    setPersonsState({ persons });
+  };
+
+  const style = {
+    backgroundColor: "pink",
+    color: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: "pointer",
+    ":hover": {
+      backgroundColor: "lightgreen",
+      color: "black"
+    }
+  };
+
+  let persons = null;
+  if (showPersonsState.showPersons) {
+    persons = (
+      <div>
+        {personsState.persons.map((person, index) => {
+          return (
+            <Person
+              key={person.id}
+              name={person.name}
+              age={person.age}
+              changed={(e) => nameChangedHandler(e, person.id)}
+              click={() => deletePersonHandler(index)}
+            />
+          );
+        })}
+      </div>
+    );
+    style.backgroundColor = "plum";
+    style[ ":hover"] = {
+      backgroundColor: "salmon",
+      color: "black"
+    };
+  }
+
+  const classes = [];
+  if(personsState.persons.length <= 2) {
+    classes.push("red");
+  }
+  if(personsState.persons.length <= 1) {
+    classes.push("burlywood");
+    console.log(classes);
+  }
+
   return (
+    <StyleRoot>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p className={classes.join(" ")}>This is really working</p>
+      <button style={style} onClick={togglePersonsHandler}>
+        Switch Name
+      </button>
+      {persons}
     </div>
+    </StyleRoot>
   );
-}
+};
 
-export default App;
+export default Radium(App);
